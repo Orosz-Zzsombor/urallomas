bf1b14e7ba6d76a4b074a951a5c1ce903bc55d7b
Orosz-Zzsombor <101347624+Orosz-Zzsombor@users.noreply.github.com>
2022-11-02 18:00:20 +0100

    Add files via upload

---
+++ űr/myscripts.js
@@ -0,0 +1,12 @@
+function Submit(){
+    document.getElementById('igazoltNev').innerHTML = document.getElementById('Megrendelo').value;
+    document.getElementById('igazoltCim').innerHTML = document.getElementById('cim').value;
+    document.getElementById('igazoltTelefon').innerHTML = document.getElementById('telefon').value;
+}
+function fokusz(){
+    hatter.style.background="yellow"
+
+}
+function homaly(){
+    document.getElementById('Megrendelo').innerHTML 
+}
